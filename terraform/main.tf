80d4a9a4a453d9c15398e15ff5f18269d321930d
Dunvael Le Roux <simplon-lerouxDunvael@gmail.com>
2023-10-31 14:34:39 +0100

    Brief_13/ adeed to path

--- terraform/main.tf
+++ terraform/main.tf
@@ -107,7 +107,7 @@ resource "azurerm_linux_virtual_machine" "VM" {
   admin_username      = var.admin_username
   admin_ssh_key {
     username   = var.admin_username
-    public_key = file("~/.ssh/id_rsa.pub") # file("~/Brief_13/.ssh/id_rsa.pub") for azure, file("~/.ssh/id_rsa.pub") for WSL
+    public_key = file("~/Brief_13/.ssh/id_rsa.pub") # file("~/Brief_13/.ssh/id_rsa.pub") for azure, file("~/.ssh/id_rsa.pub") for WSL
   }
   os_disk {
     name              = "osdisk"
